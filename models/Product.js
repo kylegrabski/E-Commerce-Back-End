ab85013e112c9487e97d9f4c29d725cd43f411a6
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-31 21:59:01 -0700

    Create model fields and rules

--- models/Product.js
+++ models/Product.js
@@ -1,22 +1,59 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
+
 Product.init(
   {
-    // define columns
+    // id INT NOT NULL OK AI
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // product_name STRING NOT NULL
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // price DECIMAL NOT NULL VALIDATES DECIMAL
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    // stock INT NOT NULL DEFAULT 10 VALIDATES INT
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    // category_id INT REFERENCES CATEGORY ID
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // id INT NOT NULL PK AI
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // tag_name STRING
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
